74d0d04ea39a355d7cc4bd71c4808a7136d6fb07
Johnny Halife <johnny@breeze.local>
2009-10-15 21:58:19 -0300

    # enhancing references

--- lib/waz-blobs.rb
+++ lib/waz-blobs.rb
@@ -7,10 +7,7 @@ require 'restclient'
 require 'hmac-sha2'
 
 $:.unshift(File.dirname(__FILE__))
-require 'waz/storage/base'
-require 'waz/storage/core_service'
-require 'waz/storage/exceptions'
-require 'waz/storage/version'
+require 'waz-storage'
 require 'waz/blobs/blob_object'
 require 'waz/blobs/container'
 require 'waz/blobs/exceptions'
