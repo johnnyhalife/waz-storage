db332ba31ae80e8cd6345c8e1a823673f02a2e7f
Johnny Halife <johnny@breeze.local>
2009-11-30 00:37:14 -0300

    # updated version to 0.5.6 for gem publishing

--- lib/waz/storage/version.rb
+++ lib/waz/storage/version.rb
@@ -3,7 +3,7 @@ module WAZ
     module VERSION #:nodoc:
       MAJOR    = '0'
       MINOR    = '5'
-      TINY     = '4' 
+      TINY     = '6' 
     end
     
     Version = [VERSION::MAJOR, VERSION::MINOR, VERSION::TINY].compact * '.'
