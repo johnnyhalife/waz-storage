c215cc640d403ee16f22eef7342d845df9fac0f1
Johnny Halife <johnny@breeze.local>
2010-02-04 20:15:02 -0300

    # v1.0 shipped

--- lib/waz/storage/version.rb
+++ lib/waz/storage/version.rb
@@ -1,9 +1,9 @@
 module WAZ
   module Storage
     module VERSION #:nodoc:
-      MAJOR    = '0'
-      MINOR    = '5'
-      TINY     = '81' 
+      MAJOR    = '1'
+      MINOR    = '0'
+      TINY     = '0' 
     end
     
     Version = [VERSION::MAJOR, VERSION::MINOR, VERSION::TINY].compact * '.'
