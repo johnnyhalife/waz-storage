ab99d87865759d7c1ccacad5a4c78ad6b0fbab10
Johnny Halife <johnny.halife@me.com>
2010-03-23 10:02:12 -0300

    # updating the readme

--- tests/waz/func/exclude.service_functional_test.rb
+++ tests/waz/func/exclude.service_functional_test.rb
@@ -13,34 +13,17 @@ require 'lib/waz-queues'
 
 describe "queues service behavior" do   
   it "should satisfy my expectations" do
-    options = { :account_name => "wazstoragejohnny", 
-                :access_key => "Tm870FVNS14aNW1zsn13fZykc4yDKz82W8m4qujIZTayOJvhOePsjSFIsFnQF8rPnDaRJQJwzhoziI7ZtIWTsQ==" }
+    options = { :account_name => "cherry", 
+                :access_key => "J9NfKPlRQLXlDnVWWk2yGOBvKmn6QR65aGVgCM75ZhegkH9ff+x9B62xHn5IE2pOIYS8i7yvL1pPAfRVlFYvQA==" }
 
     WAZ::Storage::Base.establish_connection!(options)
 
     # excepts that the metadata for the queue changes queue behaves with put
     # it performs a validation whether metadata changed or not (if changed HTTP 409 conflict)
-    queue = WAZ::Queues::Queue.find('my-queue-new4')
-    queue ||= WAZ::Queues::Queue.create('my-queue-new4')
-    queue.nil?.should == false
-    
-    queue.clear()
-    queue.size.should == 0    
-
-    10.times do |m|
-      # enqueue a receives string. Message content can be anything up to 8KB
-      # you can serialize and send anything that serializes to UTF-8 string
-      queue.enqueue!("message##{m}")
-    end
-    
-    queue.size.should == 10
-
-    # Since WAZ implements the peek lock pattern we are locking messages (not dequeuing)    
-    while(message = queue.lock) do
-      message.dequeue_count.should == 1
-      message.destroy!
-    end
-    
-    queue.size.should == 0
+    queue = WAZ::Queues::Queue.ensure('my-queue-new')
+    queue.clear
+    queue.enqueue!("Hello, world!")
+    messages = queue.lock()
+    p messages.message_text
   end
 end
