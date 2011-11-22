87e7657be9936bcdcb1ac8319d904d6db48d17c7
Johnny G. Halife <johnny.halife@me.com>
2011-11-22 17:58:42 -0300

    updated the version number to what's published right now

--- lib/waz/storage/version.rb
+++ lib/waz/storage/version.rb
@@ -3,7 +3,7 @@ module WAZ
     module VERSION #:nodoc:
       MAJOR    = '1'
       MINOR    = '1'
-      TINY     = '0' 
+      TINY     = '1' 
     end
     
     Version = [VERSION::MAJOR, VERSION::MINOR, VERSION::TINY].compact * '.'
