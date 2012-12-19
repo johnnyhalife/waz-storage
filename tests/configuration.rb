52138b6b94859dab51f768afc41c2ef114ff97c9
Bosko Ivanisevic <bosko.ivanisevic@gmail.com>
2012-12-19 12:02:31 +0100

    Move to SimpleCov and abandon Ruby 1.8 support

--- tests/configuration.rb
+++ tests/configuration.rb
@@ -1,3 +1,11 @@
+require 'simplecov'
+SimpleCov.start do
+  add_group "Gem files" do |src_file|
+    lib_path = File.expand_path("../lib", File.dirname(__FILE__))
+    src_file.filename.start_with?(lib_path)
+  end
+end
+
 require 'rubygems'
 require 'rspec'
 require 'rspec/autorun'
